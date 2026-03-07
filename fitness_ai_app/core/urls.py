93df538099ba6b769699009e92d106bfc9b9856d
Ranis <ranis.hamidullin36@gmail.com>
2026-03-07 18:00:29 +0000

    new files and log in page created (template)

--- fitness_ai_app/core/urls.py
+++ fitness_ai_app/core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.splash, name='splash'),
+    path('user_get_started/', views.user_get_started, name='user_get_started'),
+    path('user_login/', views.user_login, name='user_login'),
 ]
--- fitness_ai_app/core/views.py
+++ fitness_ai_app/core/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def splash(request):
     return render(request, 'core/splash.html')
+
+def user_get_started(request):
+    return render(request, 'core/user_get_started.html')
+
+def user_login(request):
+    return render(request, 'core/user_login.html')
