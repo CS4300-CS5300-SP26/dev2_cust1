528341ec462aef858833cea7f5e96eb6851ea8c5
Caroline <cduncan2@uccs.edu>
2026-03-11 00:30:38 +0000

    updated readme

--- fitness_ai_app/setup_and_run.sh
+++ fitness_ai_app/setup_and_run.sh
@@ -3,7 +3,8 @@
 if [ ! -d ~/venv_dev2-cust1 ]; then
   python3 -m venv ~/venv_dev2-cust1 --system-site-packages
 fi
-#TODO: Add pip command for deps
+
 source ~/venv_dev2-cust1/bin/activate
+pip install django djangorestframework pytest
 python manage.py collectstatic
 python manage.py runserver 0.0.0.0:3000
