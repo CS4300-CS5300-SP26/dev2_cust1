b0bf6b29d600dcf6936c6841f688ee5010283ba5
Ranis <ranis.hamidullin36@gmail.com>
2026-04-16 05:01:27 +0000

    fixed migration issues

--- fitness_ai_app/core/migrations/0012_userprofile_social_login_user.py
+++ fitness_ai_app/core/migrations/0012_userprofile_social_login_user.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0009_userprofile_onboarding_completed'),
+        ('core', '0011_mealsupplement'),
     ]
 
     operations = [
