0107af901970672e60c642faaeb701eab52495fa
Caroline <cduncan2@uccs.edu>
2026-04-20 14:18:21 +0000

    fixed conflicting migrations error

---
+++ fitness_ai_app/core/migrations/0017_merge_fooddatabase_and_workout_timer.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0015_fooddatabase'),
+        ('core', '0016_workout_current_session_seconds'),
+    ]
+
+    operations = [
+    ]
