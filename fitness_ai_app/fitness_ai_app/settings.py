acafea8ff50ba40f236b7ee6c335c64cd8c1d807
Zach Miller <zmiller4@uccs.edu>
2026-04-21 03:16:54 +0000

    Stop nginx and gunicorn from infinite 301'ing

--- fitness_ai_app/fitness_ai_app/settings.py
+++ fitness_ai_app/fitness_ai_app/settings.py
@@ -51,7 +51,7 @@ DEBUG = env_bool('DEBUG', True)
 
 ALLOWED_HOSTS = env_list('ALLOWED_HOSTS', 'localhost,127.0.0.1,testserver,.ondigitalocean.app')
 CSRF_TRUSTED_ORIGINS = env_list('CSRF_TRUSTED_ORIGINS')
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
